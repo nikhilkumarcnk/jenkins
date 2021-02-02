e959f3c5d13d40347d442428b8bffcb0d21c45fb
nikhil <chnikhilkumar7@gmail.com>
2021-02-02 21:15:02 +0530

    add Nik.java

--- Nik.java
+++ Nik.java
@@ -3,7 +3,7 @@ public class Nik {
     public static void main(String[] args) {
         
         int first = 10;
-        int second = 20;
+        int second = 2;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
